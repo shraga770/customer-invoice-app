393aad85d92684beda37d2ae3a1401ab86f72086
גלגלי השומרון בע"מ <shraga771@gmail.com>
2025-03-04 23:31:17 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("cache-v1").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/static/css/styles.css",
+        "/static/js/app.js",
+        "/static/icons/icon-192x192.png",
+        "/static/icons/icon-512x512.png",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
